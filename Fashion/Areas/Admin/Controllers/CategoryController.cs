81d8a3142669d56a930e67ed3f1f9e4ad67a9bfc
LuongTham <thamltgch17164@fpt.edu.vn>
2019-10-10 09:27:32 +0700

    upfiles
    some file in Fashion project

---
+++ Fashion/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fashion.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        public ActionResult ListCategory()
+        {
+            return View("ListCategory");
+        }
+    }
+}
